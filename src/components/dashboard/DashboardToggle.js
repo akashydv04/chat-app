5443e8de6a00c2cb69e77b50aaa635be43c88d46
Akash Yadav <akashydv04@gmail.com>
2023-05-02 23:15:59 +0530

    nickname updated inside the database

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
 import { useMediaQuery, useModelState } from '../../misc/custom-hook';
 import Dashboard from '.';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModelState();
 
   const isMobile = useMediaQuery('(max-width:992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('Signed out', 4000);
+    close();
+  }, [close]);
 
   return (
     <>
@@ -14,7 +21,7 @@ const DashboardToggle = () => {
         <Icon icon="dashboard"> Dashboard</Icon>
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
+import { database } from '../../misc/firebase';
 
-const Dashboard = () => {
-  return <div>asdfghjk</div>;
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  const onSave = async newData => {
+    const userNickNameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+    try {
+      await userNickNameRef.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (error) {
+      Alert.error(error.message, 4000);
+    }
+  };
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
+      </Drawer.Body>
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign Out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
