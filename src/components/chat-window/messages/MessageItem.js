81a265d512bcc8acb76f8487d689e545f3b71c30
Akash Yadav <akashydv04@gmail.com>
2023-05-07 22:49:58 +0530

    displayed user data with modal window

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProfileAvatar from '../../ProfileAvatar';
 import TimeAgo from 'timeago-react';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ message }) => {
   const { author, createdAt, text } = message;
@@ -9,13 +10,17 @@ const MessageItem = ({ message }) => {
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-bolder mb-1">
         <ProfileAvatar
-          scale="100"
           src={author.avatar}
           name={author.name}
           className="ml-1"
           size="xs"
         />
-        <span className="ml-2">{author.name}</span>
+        {/* <span className="ml-2">{author.name}</span> */}
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
---
+++ src/components/chat-window/messages/ProfileInfoBtnModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useModelState } from '../../../misc/custom-hook';
+import ProfileAvatar from '../../ProfileAvatar';
+
+const ProfileInfoBtnModal = ({ profile, ...btnProps }) => {
+  const { isOpen, open, close } = useModelState();
+  const firstName = profile.name.split(' ')[0];
+  const { name, avatar, createdAt } = profile;
+  const memberSince = new Date(createdAt).toLocaleDateString();
+  return (
+    <>
+      <Button {...btnProps} onClick={open}>
+        {firstName}
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>{firstName} Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <ProfileAvatar scale={'200'} src={avatar} name={name} />
+          <h4 className="mt-2">{name}</h4>
+          <p>Member Since: {memberSince}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfileInfoBtnModal;
